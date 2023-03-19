dac1db94801f6aa751a8d86210ebd0a57a797729
Amarusinggithub <loveamar285@gmail.com>
2023-03-18 21:31:57 -0400

    favicon added and  better code

---
+++ index.js
@@ -0,0 +1,12 @@
+const calculatorOperators = document.querySelectorAll('[data-operaters]');
+const calculatorNums = document.querySelectorAll('[data-numbers]');
+const calsulatorEqual = document.querySelector('[data-equal]');
+const calculatorDec = document.querySelector('[data-decimal]');
+const calculatorClear = document.querySelector('[data-clear]');
+
+
+class Calculator {
+    constructor() {
+
+    }
+}
